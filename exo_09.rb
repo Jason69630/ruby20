3320b1daeab8b4f986d19550cb7c1606bda5bf84
Jason69630 <52469918+Jason69630@users.noreply.github.com>
2019-07-09 23:56:00 +0200

    Add files via upload

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, quel est votre prénom ?"
+print "> " 
+first_name = gets.chomp
+puts "Quel est votre nom de famille ?"
+print "> "
+last_name = gets.chomp
+puts "Bonjour" + first_name + last_name
